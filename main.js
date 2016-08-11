095a4d8eafed7c6365ae40591a0428c97382ccac
glitchybitchy <woznarowycz@gmail.com>
2016-08-11 16:51:51 +0100

    first iteration with reddit api

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-$('body').append("<p> Hello </p>");
+var app = {};
+
+
+app.getData = function () {
+  $.ajax({
+    url: 'https://www.reddit.com/r/science/top.json',
+    dataType: 'json',
+    success: function( response ) {
+      console.log( response );
+
+      for(i = 0; i<response.data.children.length; i++ ){
+      	$('.card-columns').append('<div class="card card-block"><p class="lead">'+response.data.children[i].data.title+'</p> </div>');
+      	console.log(response.data.children[i].data.score);
+
+      	// var cardWidth = $('.card').width();
+
+      	// $('.card').css({'height':cardWidth+'px'})
+
+
+      }
+    }
+  });
+
+};
+
+
+app.init = function() {
+    $(document).ready(app.getData);
+    
+};
+
+$(document).ready(app.init);
